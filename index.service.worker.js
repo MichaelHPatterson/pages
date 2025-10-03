39b39f2188a4649e1e57be509701eca5deb374e8
Michael Patterson <mhowardpatterson@gmail.com>
2025-10-03 01:59:46 -0400

    Try changing project settings

--- index.service.worker.js
+++ index.service.worker.js
@@ -4,7 +4,7 @@
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
 /** @type {string} */
-const CACHE_VERSION = '1759470689|18745418245';
+const CACHE_VERSION = '1759471130|19186825445';
 /** @type {string} */
 const CACHE_PREFIX = 'Puzzle Flight-sw-cache-';
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
